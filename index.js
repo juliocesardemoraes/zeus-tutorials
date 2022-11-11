8879ff393ca47097e62ed6f189d434912dfda35b
julio cesar <jcmcf2012@gmail.com>
2022-11-11 12:44:18 -0300

    feat: carousel css

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const renderList = () => {
-  console.log("HERE");
-  const list = document.getElementById("nav__small__links");
-  const checkIfHide = list.classList.contains("component__hide");
+const fetchLatestMovies = async (queryParameter) => {
+  const getNewMovies = await fetch(
+    `https://api.themoviedb.org/3/movie/${queryParameter}?api_key=<CREATEYOURAPI>&language=pt-BR&page=1`,
+    { mode: "cors" }
+  );
 
-  if (checkIfHide) {
-    list.classList.remove("component__hide");
-    const body = document.getElementsByTagName("BODY")[0];
-    body.classList.add("whiteBackground");
-  } else {
-    list.classList.add("component__hide");
-    const body = document.getElementsByTagName("BODY")[0];
-    body.classList.remove("whiteBackground");
+  const post = await getNewMovies.json();
+  return post;
+};
+
+const loadMovies = async () => {
+  const popularMovies = await fetchLatestMovies("popular");
+
+  const container = document.getElementById("movie__section");
+  for (let i = 0; i < popularMovies.results.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("relative__parent");
+    const img = document.createElement("img");
+    img.src = `https://image.tmdb.org/t/p/w500/${popularMovies.results[i]?.poster_path}`;
+
+    div.append(img);
+    container.append(div);
   }
 };
