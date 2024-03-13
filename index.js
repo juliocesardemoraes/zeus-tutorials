a495fc5ee71e2013aaed7f3fe0ad13cc213b64e2
julio cesar <jcmcf2012@gmail.com>
2024-03-13 13:59:38 -0300

    feat: add multilanguage

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const renderList = () => {
-  console.log("HERE");
-  const list = document.getElementById("nav__small__links");
-  const checkIfHide = list.classList.contains("component__hide");
+import { todasAsLinguagens } from "./languages.js";
 
-  if (checkIfHide) {
-    list.classList.remove("component__hide");
-    const body = document.getElementsByTagName("BODY")[0];
-    body.classList.add("whiteBackground");
-  } else {
-    list.classList.add("component__hide");
-    const body = document.getElementsByTagName("BODY")[0];
-    body.classList.remove("whiteBackground");
-  }
+const gerarTexto = () => {
+  // 1 - PEGAR A LINGUAGEM OK
+  const idioma = document.documentElement.lang;
+
+  // 2 - MAPEAR OS TEXTOS EM UM OBJETO OK
+  // 3 - MOSTRAR NA TELA
+  const teste = document.getElementsByClassName("teste")[0];
+  teste.innerHTML = todasAsLinguagens[idioma]?.welcome;
 };
+
+window.gerarTexto = gerarTexto;
