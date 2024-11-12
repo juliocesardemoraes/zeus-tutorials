7d5a663478c233f87b6e7b387b03c85bb6b4fb1d
juliocesardemoraes <jcmcf2012@gmail.com>
2024-11-12 08:35:56 -0300

    feat: add short class

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const renderList = () => {
-  console.log("HERE");
-  const list = document.getElementById("nav__small__links");
-  const checkIfHide = list.classList.contains("component__hide");
+const mockedArray = [
+  "Ana Silva",
+  "João Santos",
+  "Maria Oliveira",
+  "Pedro Silva",
+  "Ana Silva",
+  "Lucas Pereira",
+  "Maria Oliveira",
+  "Fernanda Souza",
+  "João Santos",
+  "Lucas Pereira",
+];
 
-  if (checkIfHide) {
-    list.classList.remove("component__hide");
-    const body = document.getElementsByTagName("BODY")[0];
-    body.classList.add("whiteBackground");
-  } else {
-    list.classList.add("component__hide");
-    const body = document.getElementsByTagName("BODY")[0];
-    body.classList.remove("whiteBackground");
-  }
-};
+const studentSet = new Set(mockedArray);
+console.log("Conteúdo do Set:", [...studentSet]);
