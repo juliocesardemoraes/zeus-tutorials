9a873004f935e4183e5bbf7876b1b6f35f3790e1
juliocesardemoraes <jcmcf2012@gmail.com>
2024-11-12 09:11:33 -0300

    feat: add array x object

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-const mockedArray = [
-  "Ana Silva",
-  "João Santos",
-  "Maria Oliveira",
-  "Pedro Silva",
-  "Ana Silva",
-  "Lucas Pereira",
-  "Maria Oliveira",
-  "Fernanda Souza",
-  "João Santos",
-  "Lucas Pereira",
+const idToFind = 9999;
+const lengthAll = 10000;
+
+// Implementação O(n)
+const studentsArray = [
+  { id: 101, name: "Ana" },
+  { id: 102, name: "João" },
+  { id: 103, name: "Maria" },
+  { id: 104, name: "Pedro" },
+  { id: 105, name: "Lucas" },
+  ...Array.from({ length: lengthAll }, (_, i) => ({
+    id: i + 106,
+    name: `Student${i + 106}`,
+  })),
 ];
 
-const studentSet = new Set(mockedArray);
-console.log("Conteúdo do Set:", [...studentSet]);
+// Implementação O(1)
+const studentsObject = {};
+
+// Este for adiciona o array de estudantes
+// num formato de objeto para que seja
+// acessado mais rapidamente O(1)
+for (let i = 0; i < studentsArray.length; i++) {
+  studentsObject[studentsArray[i].id] = studentsArray[i];
+}
+
+console.log(studentsObject);
+
+// FUNÇÃO SIMPLES PARA FAZER UM FIND
+function findStudentByIdArray(id) {
+  return studentsArray.find((student) => student.id === id);
+}
+
+// FUNÇÃO SIMPLES PARA FAZER UM GET NO OBJETO
+function findStudentByIdObject(id) {
+  return studentsObject[id];
+}
+
+console.time("Array Find");
+const startArray = performance.now();
+const resultArray = findStudentByIdArray(idToFind);
+const endArray = performance.now();
+console.timeEnd("Array Find");
+
+console.time("Object Direct Access");
+const startObject = performance.now();
+const resultObject = findStudentByIdObject(idToFind);
+const endObject = performance.now();
+console.timeEnd("Object Direct Access");
+
+const timeArray = endArray - startArray;
+const timeObject = endObject - startObject;
+const percentDifference = ((timeArray - timeObject) / timeArray) * 100;
+
+console.log(
+  `Diferença de tempo: ${percentDifference.toFixed(
+    2
+  )}% mais rápido ao usar acesso direto em objeto`
+);
